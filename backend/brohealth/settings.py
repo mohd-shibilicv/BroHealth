1e48b3928325e0f17c8eb15e8e1d3b7ca23210a5
mohammed shibili cv <mohshibilicv@gmail.com>
2024-03-20 15:34:50 +0530

    Bug Fix: DEBUG and ALLOWED HOST values

--- backend/brohealth/settings.py
+++ backend/brohealth/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG")
+DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
