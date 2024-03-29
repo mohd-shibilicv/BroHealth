62d7be708a3b94f61ea50581579e357804ea5563
mohammed shibili cv <mohshibilicv@gmail.com>
2024-03-29 14:30:01 +0530

    Configure: AWS RDS

--- backend/brohealth/settings.py
+++ backend/brohealth/settings.py
@@ -118,29 +118,27 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-if not DEBUG:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("PROD_DB_NAME"),
-            "USER": os.getenv("PROD_DB_USER"),
-            "PASSWORD": os.getenv("PROD_DB_PASS"),
-            "HOST": os.getenv("PROD_DB_HOST"),
-            "PORT": os.getenv("PROD_DB_PORT"),
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("PROD_DB_NAME"),
+        "USER": os.getenv("PROD_DB_USER"),
+        "PASSWORD": os.getenv("PROD_DB_PASS"),
+        "HOST": os.getenv("PROD_DB_HOST"),
+        "PORT": os.getenv("PROD_DB_PORT"),
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.getenv("DB_NAME"),
-            "USER": os.getenv("DB_USER"),
-            "PASSWORD": os.getenv("DB_PASS"),
-            "HOST": os.getenv("DB_HOST"),
-            "PORT": os.getenv("DB_PORT"),
-        }
-    }
+}
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.getenv("DB_NAME"),
+#         "USER": os.getenv("DB_USER"),
+#         "PASSWORD": os.getenv("DB_PASS"),
+#         "HOST": os.getenv("DB_HOST"),
+#         "PORT": os.getenv("DB_PORT"),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
